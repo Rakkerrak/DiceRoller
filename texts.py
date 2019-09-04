51bf8523c6bc3d55f0d35a36788ce82d637adf43
Rakkerrak <rejcl@sbcglobal.net>
2019-09-04 16:09:44 -0400

    Started adding subclasses to the Race class. need to squish subraceMod into the parent class so its not so repetitive

---
+++ texts.py
@@ -0,0 +1,7 @@
+
+import creator
+
+HE = ("Half-elves have two points they may add to any skill.(One point per skill)")
+DW = ("Dwarves get stats based on their subrace")
+ELF = ("Elves get stats based on their subrace")
+HL = ("Halflings get stats based on their subrace")
